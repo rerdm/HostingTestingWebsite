3cf190d164d3c753ca1ddde2d48f39e7e1018692
rerdmann <reneerdmann87@web.de>
2025-07-24 11:19:18 +0200

    adding Login Page

--- fiveserver.config.js
+++ fiveserver.config.js
@@ -2,6 +2,6 @@
 module.exports = {
   // PHP executable path for Windows
   php: "C:\\xampp\\php\\php.exe",
-  open: "index.php", // Default file to open when starting the server
+  open: "index.html", // Default file to open when starting the server
   
 };
